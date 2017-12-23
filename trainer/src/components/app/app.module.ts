fac920f1da0be9d8045ae5051185d33388a281f1
Esteban Orduno <ordunoesteban@outlook.com>
2017-12-22 19:45:42 -0600

    adding exercise description and videos

--- trainer/src/components/app/app.module.ts
+++ trainer/src/components/app/app.module.ts
@@ -2,11 +2,11 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { TrainerAppComponent }  from './app.component';
-import {WorkoutRunnerComponent} from '../workout-runner/workout-runner.component';
+import {WorkoutRunnerModule} from '../workout-runner/workout-runner.module';
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [TrainerAppComponent, WorkoutRunnerComponent],
+  imports: [BrowserModule, WorkoutRunnerModule],
+  declarations: [TrainerAppComponent],
   bootstrap: [TrainerAppComponent]
 })
 export class AppModule { }
